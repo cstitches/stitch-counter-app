613803a7ee86ae5486876934de99b492a4429ce6
CE Dudley <cedudley@protonmail.com>
2021-12-02 10:35:40 -0800

    Add JS script with global variables for needed HTML elements and variables for 'count' and 'total'.

---
+++ script.js
@@ -0,0 +1,12 @@
+// variables for all needed HTML elements
+const countEl = document.getElementById("count-el");
+const incBtn = document.getElementById("increment-btn");
+const decBtn = document.getElementById("decrement-btn");
+const saveBtn = document.getElementById("save-btn");
+const clearBtn = document.getElementById("clear-btn");
+const saveTxt = document.getElementById("save-text");
+const saveTotal = document.getElementById("save-total");
+
+// initialize count as 0
+let count = 0;
+let total = 0;

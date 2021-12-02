a64afc5bc75ca1dd79b24ce3af7ce09d15d0622a
CE Dudley <cedudley@protonmail.com>
2021-12-02 10:50:56 -0800

    Add inc, dec, save, and clear functions to script, and onclick attributes to corresponding functions on buttons. Also update doc title.

--- script.js
+++ script.js
@@ -10,3 +10,41 @@ const saveTotal = document.getElementById("save-total");
 // initialize count as 0
 let count = 0;
 let total = 0;
+
+
+// increment function
+// when increment button clicked, should increase count by 1
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+}
+
+// decrement function
+// when decrement button clicked, should decrease count by 1
+function decrement() {
+    count -= 1;
+    countEl.textContent = count;
+}
+
+// save progress function
+// when save button clicked, should write current count to paragraph and reset counter to 0
+function saveProg() {
+    // each save
+    saveTxt.textContent += count + " - ";
+    // total save
+    total += count;
+    saveTotal.textContent = total;
+    // reset for next count
+    count = 0;
+    countEl.textContent = count;
+}
+
+// clear progress function
+// when clear button clicked, should reset 
+function clearProg() {
+    count = 0;
+    countEl.textContent = count;
+    saveTxt.textContent = "";
+    saveTotal.textContent = "";
+}
+
